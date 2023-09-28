7fabaf541bb5abb96c8f8d3a6d4bebb375f2ed12
Shubham <shubham145000@gmail.com>
2023-09-28 11:35:18 +0530

    day1

---
+++ day1/datatypes.js
@@ -0,0 +1,36 @@
+console.log(typeof 20);
+console.log(typeof 'shubham');
+console.log(typeof true);
+console.log(typeof s);
+
+
+const x = "";
+const y = "";
+const z = x + y;
+console.log(z);
+
+const x1 = '2';
+const y2 = '3';
+const z2 = '4';
+const p = '5';
+const Q = x + y * z + p;
+console.log(Q);
+
+
+let firstName = 'shubham'
+let lastName = 'kumar'
+let country = 'INDIA'
+let age = '20'
+let marital = 'No'
+
+
+
+// . Declare variables to store your first name, last name, marital status, country
+// and age in a single line
+let fName = "SHUBHAM", lName = "KUMAR", maritalStatus = "Single", country1 = "INDIA", age1 = 20;
+
+
+
+let myAge = 'I am 20 years old'
+let yourAge = ' You are 30 years old'
+console.log(myAge, yourAge);
---
+++ day1/variable.js
@@ -0,0 +1,30 @@
+// comment can make code readable
+// welcome  to 30daysof javaScript
+// comments can make code readable, 
+// easy to reuse and informative
+
+//  variable-----------------
+const x = 2;
+const y = 7;
+const z = x + y;
+console.log(z);
+// ----------------------string----------
+const string = "My name is Shubham"
+console.log(string);
+
+// -----------------Boolean------------------
+var ans = true;
+console.log(ans);
+
+// undefine and null data type---------------
+var temp = ['a', 'b', 'c'];
+if (temp[4] === undefined)
+    console.log("true");
+else
+    console.log("false");
+
+var nullvar = null;
+if (nullvar === null)
+    console.log("nullvar is null");
+else
+    console.log("nullvar is not null");
